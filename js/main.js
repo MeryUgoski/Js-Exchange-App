b56092cac8893fb6cde2f9605fd6804415e70471
Mery Ugoski <meryugoski@gmail.com>
2019-07-08 20:42:29 -0300

    AJAX jsoN api done

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// jQuery
+$(document).ready(function(){
+
+	const api ='https://currencydatafeed.com/api/data.php?token=kxs4oy9vjdy2lvzg2vfq&currency=USD/BRL';
+	 const botao = $('button');
+	 let resultado = $('#resultado');
+
+	 botao.click(function(){
+	 	let valor = $('#valor').val();
+	 	$.getJSON(api, function(data){
+	 		let val = data.currency[0].value;
+	 		let calculo = val*valor;
+       	 resultado.html(`<p>${calculo.toFixed(2)}</p>`);
+    
+    	});
+    });
+
+});
+// end jQuery
